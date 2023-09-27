a95302eb6203e6e815a4514fb5e146e74356e4a9
Mateusz Lech <mtslech@gmail.com>
2023-09-27 17:35:35 +0200

    code cleaning

--- product-backend/src/main/java/org/kainos/ea/service/AuthService.java
+++ product-backend/src/main/java/org/kainos/ea/service/AuthService.java
@@ -9,8 +9,8 @@ import org.kainos.ea.model.User;
 import java.sql.SQLException;
 
 public class AuthService {
-    private AuthDao authDao;
-    private AuthValidator authValidator;
+    private final AuthDao authDao;
+    private final AuthValidator authValidator;
 
     public AuthService(AuthDao authDao, AuthValidator authValidator) {
         this.authDao = authDao;
@@ -18,13 +18,12 @@ public class AuthService {
     }
 
     public String register(User user) throws FailedToRegisterException, InvalidEmailException, InvalidPasswordException {
-
         try {
-            if (!authValidator.isValidEmail(user.getEmail())) {
+            if (!AuthValidator.isValidEmail(user.getEmail())) {
                 throw new InvalidEmailException();
             }
 
-            if (!authValidator.isValidPassword(user.getPassword())) {
+            if (!AuthValidator.isValidPassword(user.getPassword())) {
                 throw new InvalidPasswordException();
             }
 
