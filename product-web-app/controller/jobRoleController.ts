832bd90c0e2684bead17509f53ebcad66f4d791b
Adrian Nowicki <adrian.nowicki@kainos.com>
2023-09-29 10:36:08 +0200

    fix: npm lint

--- product-web-app/controller/jobRoleController.ts
+++ product-web-app/controller/jobRoleController.ts
@@ -3,14 +3,14 @@ import logger from '../service/logger.js';
 import JobRoleService from '../service/jobRoleService.js';
 
 export default class JobRoleSingleViewController {
-  private jobRoleSingleViewService = new JobRoleService();
+  private JobRoleService = new JobRoleService();
 
   appRoutes(app: Application) {
     app.get('/job-roles/:id', async (req: Request, res: Response) => {
       let data = {};
 
       try {
-        data = await this.jobRoleSingleViewService.getJobRoleSpecification(Number.parseInt(req.params.id, 10));
+        data = await this.JobRoleService.getJobRoleSpecification(Number.parseInt(req.params.id, 10));
       } catch (e) {
         logger.error(`Couldnt get job Role! Error: ${e}`);
       }
