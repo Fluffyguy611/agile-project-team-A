ceed5a3f8e60d8beb879455125f542f3d7ab3a9f
Emilia Brzezińska <94071705+emiliabrzezinska@users.noreply.github.com>
2023-10-05 11:43:33 +0200

    fix: update JobRoleDao.java

--- product-backend/src/main/java/org/kainos/ea/db/JobRoleDao.java
+++ product-backend/src/main/java/org/kainos/ea/db/JobRoleDao.java
@@ -6,8 +6,6 @@ import org.kainos.ea.model.JobRole;
 import org.kainos.ea.model.JobRoleRequest;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 public class JobRoleDao {
@@ -18,37 +16,36 @@ public class JobRoleDao {
         databaseConnector = new DatabaseConnector();
     }
 
-    public List<JobRole> getAllJobRoles() throws SQLException, DatabaseConnectionException {
+    public Optional<JobRole> getAllJobRoles() throws SQLException, DatabaseConnectionException {
         Connection c = databaseConnector.getConnection();
         Statement st = c.createStatement();
         ResultSet rs = st.executeQuery("SELECT JobRole.Id, JobRole.Name, JobRole.Description, JobRole.SharePointLink, JobRole.CapabilityId\n" + "FROM JobRole");
 
-        List<JobRole> jobRoleList = new ArrayList<>();
+        
         while (rs.next()) {
-            JobRole jobRole = new JobRole(
+            Optional.of (
+                new JobRole(
                     rs.getInt("Id"),
                     rs.getString("Name"),
                     rs.getString("Description"),
                     rs.getString("SharePointLink"),
-                    rs.getInt(1));
-            jobRoleList.add(jobRole);
+            ));
         }
-        return jobRoleList;
+        return Optional.empty();    
     }
 
 
     public Optional<JobRole> createNewJobRole(JobRoleRequest jobRole) throws SQLException {
         Connection c = databaseConnector.getConnection();
 
-        String insertStatement = "INSERT INTO JobRole (Name, Description, SharePointLink, CapabilityId) VALUES (?,?,?,?)";
+        String insertStatement = "INSERT INTO JobRole (Name, Description, SharePointLink) VALUES (?,?,?,?)";
 
         PreparedStatement st = c.prepareStatement(insertStatement, Statement.RETURN_GENERATED_KEYS);
 
         st.setString(1, jobRole.getName());
         st.setString(2, jobRole.getDescription());
         st.setString(3, jobRole.getSharePointLink());
-        st.setInt(4, jobRole.getCapabilityId());
-
+        
         st.executeUpdate();
 
         ResultSet rs = st.getGeneratedKeys();
@@ -60,7 +57,7 @@ public class JobRoleDao {
                             jobRole.getName(),
                             jobRole.getDescription(),
                             jobRole.getSharePointLink(),
-                            jobRole.getCapabilityId()));
+            ));
         }
         return Optional.empty();
     }
@@ -78,7 +75,6 @@ public class JobRoleDao {
                     rs.getString("Name"),
                     rs.getString("Description"),
                     rs.getString("SharePointLink"),
-                    rs.getInt("CapabilityId")
             ));
         }
         return Optional.empty();
@@ -97,7 +93,6 @@ public class JobRoleDao {
                     rs.getString("Name"),
                     rs.getString("Description"),
                     rs.getString("SharePointLink"),
-                    rs.getInt("CapabilityId")
             ));
         }
         return Optional.empty();
