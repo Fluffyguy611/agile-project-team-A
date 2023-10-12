74a5180194250fc085595f38eb7187260dcb9419
Emilia Brzezinska <emilia.brzezinska@gmail.com>
2023-10-12 12:35:07 +0200

    fix: changing mesage in InvalidCapabilityException

--- product-backend/src/main/java/org/kainos/ea/exception/InvalidCapabilityLeadException.java
+++ product-backend/src/main/java/org/kainos/ea/exception/InvalidCapabilityLeadException.java
@@ -2,6 +2,6 @@ package org.kainos.ea.exception;
 
 public class InvalidCapabilityLeadException extends Exception {
     public InvalidCapabilityLeadException(String message) {
-        return "Failed to create capability";
+        super(message);
     }
 }
